3ab3d0d880e8a68553fad79dffc8708775edc299
xerion500321 <32055771+xerion500321@users.noreply.github.com>
2017-09-25 20:50:59 +0800

    20170925

---
+++ Main3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main3  {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        char chOld = scn.next().charAt(0);
+        char chNew = scn.next().charAt(0);
+        System.out.println(str.replace(chOld , chNew));
+    }
+}
